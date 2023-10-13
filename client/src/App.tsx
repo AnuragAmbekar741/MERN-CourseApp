c71b8fd9f0f25e2231dedcda7fd8c7911d3c4048
Anurag <anuragambekar1997@gmail.com>
2023-10-13 22:15:42 +0530

    store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import Login from "./components/shared/Login"
 
 function App() {
   return (
-    <div>   
+    <div className="bg-gradient-to-r from-[#12100E] to-[#2B4162] w-screen h-screen">   
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login/>}/>
--- client/src/components/shared/Login.tsx
+++ client/src/components/shared/Login.tsx
@@ -36,7 +36,7 @@ const Login: React.FC = () => {
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
             alt="Your Company"
           /> */}
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2 className="mt-10 text-center text-3xl font-medium leading-9 tracking-tight text-white">
             Sign in to your account
           </h2>
         </div>
@@ -44,12 +44,12 @@ const Login: React.FC = () => {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form onSubmit={(e) => handleSubmit(e)} className="space-y-6">
             <div>
-              <label className="block text-sm font-medium leading-6 text-gray-900">
+              <label className="block text-sm font-light leading-6 text-white">
                 Email address
               </label>
               <div className="mt-2">
                 <input
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 focus:outline-none px-3"
+                  className="block w-full rounded-md border-0 py-2 text-black shadow-sm  placeholder:text-gray-400 sm:text-sm sm:leading-6 focus:outline-none px-3"
                   onChange={(e) =>
                     setUserCred({ ...userCred, username: e.target.value })
                   }
@@ -59,18 +59,18 @@ const Login: React.FC = () => {
 
             <div>
               <div className="flex items-center justify-between">
-                <label className="block text-sm font-medium leading-6 text-gray-900">
+                <label className="block text-sm font-light leading-6 text-white">
                   Password
                 </label>
                 <div className="text-sm">
-                  <a className="font-semibold text-indigo-600 hover:text-indigo-500">
+                  <a className="font-semibold text-indigo-400 hover:text-indigo-500">
                     Forgot password?
                   </a>
                 </div>
               </div>
               <div className="mt-2">
                 <input
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 focus:outline-none px-3"
+                  className="block w-full rounded-md border-0 py-2 text-black shadow-sm  placeholder:text-gray-400 sm:text-sm sm:leading-6 focus:outline-none px-3"
                   onChange={(e) =>
                     setUserCred({ ...userCred, password: e.target.value })
                   }
@@ -80,14 +80,13 @@ const Login: React.FC = () => {
             <div>
               <button 
                 type="submit"
-                className="w-full bg-indigo-600 p-3 rounded-md text-white font-light"
+                className="w-full bg-indigo-500 p-3 rounded-md text-white font-light"
             >Sign In</button>
             </div>
           </form>
-
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?
-            <a className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            <a className="font-semibold leading-6 text-indigo-400 hover:text-indigo-500 mx-2">
               Start a 14 day free trial
             </a>
           </p>
