3c74d95574511652a6e2a30f9c5ceb3f8312e3c4
Anurag <anuragambekar1997@gmail.com>
2023-10-23 23:49:51 +0530

    logout

--- client/src/components/Appbar.tsx
+++ client/src/components/Appbar.tsx
@@ -2,12 +2,19 @@ import React, { useState } from 'react'
 import { adminAppbarlinks } from '../constants/data'
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { BiLogOut } from 'react-icons/bi';
-
+import { useNavigate } from 'react-router-dom';
 
 
 const Appbar:React.FC = () => {
 
     const [collapse,setCollapse] = useState(false)
+
+    const navigate = useNavigate()
+
+    const logout = () =>{
+        localStorage.setItem('token','')
+        navigate('/login')
+    }
 
   return (
     <div className={`${collapse?'w-24 rounded-l-md rounded-b-md':'w-1/5 rounded-md'} flex flex-col p-2 text-white text-md font-light  relative shadow-lg border border-gray-600`}>
@@ -30,7 +37,10 @@ const Appbar:React.FC = () => {
                 )
             })
         }
-        <div className='w-full flex -mb-80 py-2 px-5 justify-start items-center my-4 rounded-md cursor-pointer'>
+        <div 
+            className='w-full flex -mb-80 py-2 px-5 justify-start items-center my-4 rounded-md cursor-pointer'
+            onClick={logout}
+        >
             <BiLogOut className={`text-white ${collapse?'text-[2.5rem]':'text-4xl'}`}/>
             <p className={`ml-7 font-medium ${collapse?'hidden':''}`}>Logout</p>
         </div>
